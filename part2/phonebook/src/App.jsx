ae909fe3d622c2dcaacd5fd754dc25be595290b5
Rodrigo Hernández Burad <rhernand@tec.mx>
2025-08-14 21:42:11 -0600

    Exercise 2.11 Phonebook Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { useEffect } from 'react'
 
 const Filter = ({ filter, handleFilterChange }) => {
   return (
@@ -37,15 +39,17 @@ const Persons = ({ filteredPersons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [filter, setNewFilter] = useState('')
   const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => setPersons(response.data))
+  }, [])
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
